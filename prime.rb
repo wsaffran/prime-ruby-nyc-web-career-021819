19365a0c6ee81992a1faf683db09b2ac9b2bc5e0
Will Saffran <willh.saffran@gmail.com>
2019-02-05 19:22:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,12 @@ def prime?(x)
   if x <= 1
     return false
   
-  let i = 2
-  while i < x
-    if x % i == 0 
-      return false
-    i++
+  if x > 1
+    let i = 2
+    while i < x
+      if x % i == 0 
+       return false
+      i++
+    end
   return true
 end

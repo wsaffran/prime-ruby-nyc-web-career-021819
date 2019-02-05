df9f19a0c7208211f82d95d7915eb72fa26ad3b8
Will Saffran <willh.saffran@gmail.com>
2019-02-05 19:39:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,16 @@
 def prime?(x)
   
-  is_prime = true         # starting boolean variable
+  is_prime = true         # variable for boolean assignment
   
-  if x <= 1
+  if x <= 1               # if x <= 1 --> not a prime number
     is_prime = false
   end
-  for i in 2..x-1
+  
+  for i in 2..x-1         # if x % i for i = 2 through (x-1) --> not a prime number
     if x % i == 0 
       is_prime = false
     end
   end
-  return is_prime
+  
+  return is_prime         # if tests fail --> prime number
 end
